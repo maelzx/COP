b54814004384f44ba91129e010d18be7bb1444ae
ismail <maelzx@gmail.com>
2020-08-05 14:56:46 +0800

    add api endpoint (WIP)

---
+++ modul/getdata.js
@@ -0,0 +1,82 @@
+class Getdata {
+
+    constructor () {
+        this.USERCONFIGDIR = process.env.USERCONFIGDIR
+        this.PAYMENTCONFIGDIR = process.env.PAYMENTCONFIGDIR
+        this.STRIPEPAYMENTCONFIGDIR = process.env.STRIPEPAYMENTCONFIGDIR
+        this.jsonfile = require('jsonfile')
+    }
+
+    getData(type, key) {
+
+        let path = ''
+
+        switch (type) {
+            case "USER":
+                path = this.USERCONFIGDIR
+                break
+            case "PAYMENT":
+                path = this.PAYMENTCONFIGDIR
+                break
+            case "STRIPE":
+                path = this.STRIPEPAYMENTCONFIGDIR
+                break
+            default:
+                break
+
+        }
+
+        const file_location = path.join(__dirname, '..', path)
+        const file = file_location + '/' + key + '.json'
+  
+        return this.jsonfile.readFileSync(file)
+    }
+
+    setData(type, id, data) {
+
+        let path = ''
+
+        switch (type) {
+            case "USER":
+                path = this.USERCONFIGDIR
+                break
+            case "PAYMENT":
+                path = this.PAYMENTCONFIGDIR
+                break
+            case "STRIPE":
+                path = this.STRIPEPAYMENTCONFIGDIR
+                break
+            default:
+                break
+
+        }
+
+        const file_location = path.join(__dirname, '..', path)
+        const file = file_location + '/' + id + '.json'
+       
+        this.jsonfile.writeFileSync(file, data)
+
+        // TODO: check before return true
+        return true
+
+    }
+
+    getUser(key) {
+        return this.getData("USER", key)
+    }
+
+    getPayment(key) {
+        return this.getData("PAYMENT", key)
+    }
+
+    setPayment(id, data) {
+        return this.setData("PAYMENT", id, data)
+    }
+
+    getStripe(key) {
+        return this.getData("STRIPE", key)
+    }
+
+}
+
+module.exports = Getdata
---
+++ routes/api.js
@@ -0,0 +1,123 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const router = express.Router();
+const path = require('path');
+const jsonfile = require('jsonfile')
+const crypto = require('crypto')
+const axios = require('axios')
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const jsonParser = bodyParser.json()
+
+const Telegrambot = require("../modul/telegrambot.js")
+const Getdata = require("../modul/getdata.js")
+
+const telegrambot = new Telegrambot(process.env.TGBOTKEY, process.env.TGBOTAPIURL, axios)
+const getdata = new Getdata()
+
+const api_secret_key = process.env.APISECRETKEY
+
+function create_token(username) {
+
+    const random_number = Math.floor(Math.random() * 10000)
+
+    let hash = crypto.createHash('md5').update(username + api_secret_key + random_number).digest("hex")
+    
+    return random_number + ":" + hash
+
+}
+
+function verify_token(username, number, hash) {
+
+    let hash_temp = crypto.createHash('md5').update(username + api_secret_key + number).digest("hex")
+
+    return hash == hash_temp
+
+}
+
+function check_token(token) {
+
+    const {username, number, hash} = token.split(":")
+
+    if (verify_token(username, number, hash)) {
+
+        return true
+    }
+
+    return false
+
+}
+
+function create_payid(username) {
+
+    let hash = crypto.createHash('md5').update(username + Date.now()).digest("hex")
+    
+    return hash.substr(0,12)
+
+}
+
+router.post('/authenticate', jsonParser, async function(req, res) {
+
+    const username = req.body.username
+    const password = req.body.password
+
+    const member_config = getdata.getUser(username)
+
+    if (login(username, password)) {
+
+        res.json({"token": username + ":" + create_token(username)})
+
+    } else {
+
+        res.json({"error": "not valid"})
+
+    }
+
+})
+
+router.post('/create', jsonParser, async function(req, res) {
+
+    const buyerName = req.body.buyerName
+    const buyerName = req.body.buyerPhoneNo
+    const buyerName = req.body.buyerEmail
+    const itemDescription = req.body.itemDescription
+    const itemPrice = req.body.itemPrice
+    const ccEnable = req.body.ccEnable
+    const ccExtraCharge = req.body.ccExtraCharge
+    const token = req.body.token
+
+    if (!check_token(token)) {
+        res.json({"error": "token not valid"})
+    }
+
+    const {username, number, hash} = token.split(":")
+
+    const member_config = getdata.getUser(username)
+
+    const data = {
+        item_description: itemDescription,
+        item_price: itemPrice * 100,
+        name: buyerName,
+        phone_no: buyerPhoneNo,
+        email: buyerEmail,
+        agent_name: member_config.agent_name,
+        username: member_config.username,
+        seller_email: member_config.email,
+        cc_enable: ccEnable,
+        cc_charge_extra: ccExtraCharge
+    }
+
+    const payid = create_payid(member_config.username)
+
+    getdata.setPayment(payid, data)
+
+    const url = process.env.SYSTEMURL + 'pay/' + payid
+
+    res.json({"paymentLinkUrl": url})
+
+})
+
+
+
+//Return router
+module.exports = router
